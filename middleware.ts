b34811ff93350824581d39719c159e03fdfaf5e6
Sigersa <innovacionessigersa@gmail.com>
2026-01-06 17:29:10 -0600

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const session = req.cookies.get('session');
+
+  if (!session) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/system-admin-dashboard/:path*',
+    '/administrative-personnel-dashboard/:path*'
+  ]
+};
