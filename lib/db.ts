36afc46b16bd0e9b9fcc301a10bce355004e1797
Sigersa <innovacionessigersa@gmail.com>
2026-02-03 10:42:26 -0600

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -2,12 +2,17 @@ import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT), // 🔥 IMPORTANTE
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false, // 🔥 CLAVE para Railway
+  },
+
   waitForConnections: true,
-  connectionLimit: 5, // mejor para serverless
+  connectionLimit: 3, // recomendado en Vercel
   queueLimit: 0,
 });
 
