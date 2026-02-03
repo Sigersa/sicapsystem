c2b7638cf29240427434d7244a7485d9fc21c972
Sigersa <innovacionessigersa@gmail.com>
2026-02-03 10:39:36 -0600

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,18 @@
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool ({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT), // 🔥 IMPORTANTE
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 5, // mejor para serverless
+  queueLimit: 0,
 });
 
-export async function  getConnection() {
-    return pool.getConnection();
+export async function getConnection() {
+  return pool.getConnection();
 }
 
 export default pool;
