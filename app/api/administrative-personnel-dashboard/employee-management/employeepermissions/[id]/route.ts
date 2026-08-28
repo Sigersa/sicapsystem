7ae4c13a71553adad516f851e797803886f01f39
Sigersa <innovacionessigersa@gmail.com>
2026-08-28 17:34:13 -0600

    Update route.ts

--- app/api/administrative-personnel-dashboard/employee-management/employeepermissions/[id]/route.ts
+++ app/api/administrative-personnel-dashboard/employee-management/employeepermissions/[id]/route.ts
@@ -378,7 +378,7 @@ async function generateUpdatedPermissionPDF(
     ws.getCell("P11").value = applicationYear;
     ws.getCell("C13").value = employeeName || "NO ESPECIFICADO";
     ws.getCell("C14").value = perm.AreaOrProject || "NO ESPECIFICADO";
-    ws.getCell("C15").value = perm.tipo || "NO ESPECIFICADO";
+    ws.getCell("C15").value = perm.Position || "NO ESPECIFICADO";
     ws.getCell("M13").value = departureTime;
     ws.getCell("M14").value = checkInTime;
     ws.getCell("M15").value = perm.DaysOfLeave ? perm.DaysOfLeave.toString() : "NO ESPECIFICADO";
