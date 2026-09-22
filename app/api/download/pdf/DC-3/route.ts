a31e4bd9747b86f3ab5ad392cf367833b6c1ba0c
Sigersa <innovacionessigersa@gmail.com>
2026-09-22 15:55:55 -0600

    Update route.ts

--- app/api/download/pdf/DC-3/route.ts
+++ app/api/download/pdf/DC-3/route.ts
@@ -163,7 +163,7 @@ export async function GET(request: NextRequest) {
     const templatePath = path.join(
       process.cwd(),
       "public",
-      "administrative-personnel-dashboard",
+      "human-resources-dashboard",
       "personnel-management",
       "DC-3.xlsx"
     );
