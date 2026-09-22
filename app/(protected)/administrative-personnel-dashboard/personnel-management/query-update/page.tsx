348d038576fbf974717d61224e0fbd1352aad1ea
Sigersa <innovacionessigersa@gmail.com>
2026-09-22 15:09:07 -0600

    Update page.tsx

--- app/(protected)/administrative-personnel-dashboard/personnel-management/query-update/page.tsx
+++ app/(protected)/administrative-personnel-dashboard/personnel-management/query-update/page.tsx
@@ -1170,7 +1170,7 @@ export default function EmployeesListPage() {
 
   return (
     <div className="min-h-screen bg-gray-100">
-      <AppHeader title="PANEL ADMINISTRATIVO" />
+      <AppHeader title="PANEL DE RECURSOS HUMANOS" />
       <main className="pt-[72px] pb-[80px] min-h-screen bg-gray-100">
         <div className="w-full px-3 sm:px-4 md:px-6 py-4 sm:py-6 md:py-8 max-w-7xl mx-auto">
           <div className="mb-6">
