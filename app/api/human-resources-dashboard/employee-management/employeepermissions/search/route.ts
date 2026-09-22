588308c4881d4d6b159c18d3908d1c70c069b0a7
Sigersa <innovacionessigersa@gmail.com>
2026-09-22 15:54:55 -0600

    Update route.ts

--- app/api/human-resources-dashboard/employee-management/employeepermissions/search/route.ts
+++ app/api/human-resources-dashboard/employee-management/employeepermissions/search/route.ts
@@ -1,5 +1,3 @@
-// app/api/administrative-personnel-dashboard/employee-management/employeepermissions/search/route.ts
-
 import { NextRequest, NextResponse } from 'next/server';
 import { getConnection } from "@/lib/db";
 import { validateAndRenewSession } from "@/lib/auth";
