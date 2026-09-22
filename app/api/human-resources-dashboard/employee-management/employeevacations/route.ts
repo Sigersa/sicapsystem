819e36766167da032c5eacb0bc8aecc521254216
Sigersa <innovacionessigersa@gmail.com>
2026-09-22 15:54:53 -0600

    Update route.ts

--- app/api/human-resources-dashboard/employee-management/employeevacations/route.ts
+++ app/api/human-resources-dashboard/employee-management/employeevacations/route.ts
@@ -1,4 +1,3 @@
-// app/api/administrative-personnel-dashboard/employee-management/employeevacations/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { getConnection } from "@/lib/db";
 import { validateAndRenewSession } from "@/lib/auth";
