4c895a22c83de2c71183bb53719830d7aa9c276b
Sigersa <innovacionessigersa@gmail.com>
2026-09-22 15:55:57 -0600

    Update route.ts

--- app/api/download/edit/FT-RH-29/route.ts
+++ app/api/download/edit/FT-RH-29/route.ts
@@ -138,7 +138,7 @@ export async function GET(request: NextRequest) {
     const templatePath = path.join(
       process.cwd(),
       "public",
-      "administrative-personnel-dashboard",
+      "human-resources-dashboard",
       "hiring",
       "FT-RH-29.docx"
     );
