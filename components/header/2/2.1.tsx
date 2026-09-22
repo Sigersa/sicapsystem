1a8965b45782accc733040a7ba574edce2074ee4
Sigersa <innovacionessigersa@gmail.com>
2026-09-22 15:07:55 -0600

    Update 2.1.tsx

--- components/header/2/2.1.tsx
+++ components/header/2/2.1.tsx
@@ -122,7 +122,7 @@ export default function AppHeader({
           </div>
         </div>
 
-        {/* Botón Principal, Notificaciones y usuario */}
+        {/* Botón Principal */}
         <div className="flex items-center space-x-4">
 
           {/* Botón Principal */}
